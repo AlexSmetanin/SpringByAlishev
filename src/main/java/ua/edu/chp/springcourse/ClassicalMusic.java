1ca6c116239ac858e3897399006835321c553f13
Alex Smetanin <AlexSmetanin72@gmail.com>
2022-07-04 17:20:07 +0300

    Spring Framework. Урок 8: Жизненный цикл бина (Bean Lifecycle). Init, Destroy и Factory методы.

--- src/main/java/ua/edu/chp/springcourse/ClassicalMusic.java
+++ src/main/java/ua/edu/chp/springcourse/ClassicalMusic.java
@@ -1,6 +1,21 @@
 package ua.edu.chp.springcourse;
 
 public class ClassicalMusic implements Music {
+
+    private ClassicalMusic() {} // приватный конструктор, чтоб нельзя было создать объект командой new, а только через фабричный метод
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit() {
+        System.out.println("Do my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Do my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
